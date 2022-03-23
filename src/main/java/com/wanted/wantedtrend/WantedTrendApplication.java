9ac97a5e7aa73725a92a1ef80df5fc5b917236d8
Zagg2732 <67727906+Zagg2732@users.noreply.github.com>
2022-03-23 11:41:04 +0900

    Initial commit

---
+++ src/main/java/com/wanted/wantedtrend/WantedTrendApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.wantedtrend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedTrendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedTrendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanted/wantedtrend/WantedTrendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.wantedtrend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedTrendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
