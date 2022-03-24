628ec45ccaf6ac1a9c21d432b2947746c05de77b
Zagg2732 <67727906+Zagg2732@users.noreply.github.com>
2022-03-24 18:19:30 +0900

    add header, main(router), bootstrapVue, bootstrap5 template example

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <HeaderTitle />
   <router-view/>
 </template>
 
+<script>
+import HeaderTitle from "@/layouts/HeaderTitle";
+
+export default {
+  components: {HeaderTitle}
+}
+</script>
+
 <style>
+
+/* css */
+@import "assets/templates/css/style.css";
+
+@import "assets/templates/vendor/boxicons/css/boxicons.min.css";
+@import "assets/templates/vendor/quill/quill.snow.css";
+@import "assets/templates/vendor/quill/quill.bubble.css";
+@import "assets/templates/vendor/remixicon/remixicon.css";
+@import "assets/templates/vendor/simple-datatables/style.css";
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
