03a3845503183e84842efa5a2701e63bc409c997
Zagg2732 <67727906+Zagg2732@users.noreply.github.com>
2022-03-30 15:22:39 +0900

    modified headerTitle, add VueApexCharts, style.css, testdata for charts, temp logo.png

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueApexCharts from 'vue3-apexcharts'
 
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
 app.use(router)
+app.use(VueApexCharts)
 app.mount('#app')
