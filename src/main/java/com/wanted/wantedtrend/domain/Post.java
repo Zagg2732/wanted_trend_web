bdc2081cef7674ad796f4088bb3868b21281914d
zagg2732@gmail.com <zagg2732@gmail.com>
2022-04-04 02:24:39 +0900

    add JPA, Post class, Service, JpaRepository

---
+++ src/main/java/com/wanted/wantedtrend/domain/Post.java
@@ -0,0 +1,29 @@
+package com.wanted.wantedtrend.domain;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url;         // wd url
+
+    private Date date;          // 올린날짜
+
+    private String location;    // 지역
+
+    private String requirement; // 요구사항
+
+    private String prefer;      // 우대사항
+
+    @Column(name = "company_name")
+    private String companyName; // 회사이름
+
+    @Column(name = "main_task")
+    private String mainTask;    // 주요업무
+
+}
