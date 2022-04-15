fc480530388976554198b2abdf08e3d5ad865b6f
zagg2732@gmail.com <zagg2732@gmail.com>
2022-04-16 03:14:40 +0900

    ADD chart_data.json for init project

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,21 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   outputDir: '../src/main/resources/static',
-//   publicPath: '/',
-//   devServer:{
-//     proxy: {
-//       port: 3000,
-//       '^/api' : {
-//         target: {
-//           host: 'localhost',
-//           protocol: 'http',
-//           port: 8083
-//         },
-//         ws: true,
-//         changeOrigin: true
-//       }
-//     }
-//   }
-//
-// })
+module.exports = defineConfig({
+  transpileDependencies: true,
+  outputDir: '../src/main/resources/static',
+  publicPath: '/',
+  devServer:{
+    proxy: {
+      port: 3000,
+      '^/api' : {
+        target: {
+          host: 'localhost',
+          protocol: 'http',
+          port: 8080
+        },
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+
+})
